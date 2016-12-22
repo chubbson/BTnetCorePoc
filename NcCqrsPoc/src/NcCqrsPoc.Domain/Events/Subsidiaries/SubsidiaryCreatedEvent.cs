0efbfbfc20df4aa4b0c70e75d04da86e0f5d2fba
chubbson <chubbson@gmail.com>
2016-12-22 23:58:23 +0100

    subsidiarycreated event added

---
+++ NcCqrsPoc/src/NcCqrsPoc.Domain/Events/Subsidiaries/SubsidiaryCreatedEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Domain.Events.Subsidiaries
+{
+    public class SubsidiaryCreatedEvent : BaseEvent
+    {
+        public readonly int SubsidiaryID;
+        public readonly string StreetAddress;
+        public readonly string City;
+        public readonly string PostalCode;
+
+        public SubsidiaryCreatedEvent(Guid id, int subsidiaryID, string streetAddress, string city, string state, string postalCode)
+        {
+            Id = id;
+            SubsidiaryID = subsidiaryID;
+            StreetAddress = streetAddress;
+            City = city;
+            PostalCode = postalCode;
+        }
+    }
+}
