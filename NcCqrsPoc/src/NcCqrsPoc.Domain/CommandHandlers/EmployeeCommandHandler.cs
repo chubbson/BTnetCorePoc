72cb6e8ff22a6974bab323b006cde6d986249ec2
chubbson <chubbson@gmail.com>
2016-12-22 23:11:23 +0100

    Added EmployeeCommandHandler (inherit form CQRSlite Commands ICommandHandler) for CreateEmployeeCommand skeleton

---
+++ NcCqrsPoc/src/NcCqrsPoc.Domain/CommandHandlers/EmployeeCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using CQRSlite.Commands;
+using NcCqrsPoc.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Domain.CommandHandlers
+{
+    public class EmployeeCommandHandler : ICommandHandler<CreateEmployeeCommand>
+    {
+        public void Handle(CreateEmployeeCommand message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
