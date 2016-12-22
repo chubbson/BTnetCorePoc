785721388050641762e29572282dfef251bf819e
chubbson <chubbson@gmail.com>
2016-12-22 23:57:33 +0100

    Subsidiary AggregateRoot added, Constructor does throw not implemented exception, because subsidiarycreated event doesnt exists yet

---
+++ NcCqrsPoc/src/NcCqrsPoc.Domain/WriteModel/AggrRoot/Subsidiary.cs
@@ -0,0 +1,38 @@
+﻿using CQRSlite.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Domain.WriteModel.AggrRoot
+{
+    /// <summary>
+    /// Subsidiary Aggregate Root class 
+    /// interprete a physical location or 'Filiale'
+    /// contains a list registred employees
+    /// </summary>
+    public class Subsidiary : AggregateRoot
+    {
+        private int _subsidiaryID;
+        private string _streetAddress;
+        private string _city;
+        private string _postalCode;
+        private List<int> _employees;
+
+        // private default constructor
+        private Subsidiary() { }
+
+        public Subsidiary(Guid id, int subsidiaryID, string streetAddress, string city, string postalCode)
+        {
+            Id = id;
+            _subsidiaryID = subsidiaryID;
+            _streetAddress = streetAddress;
+            _city = city;
+            _postalCode = postalCode;
+            _employees = new List<int>();
+
+            throw new NotImplementedException("Apply Change new Subsidiary CreatedEvent is not implementet in Subsidiary Constructor");
+            // apply Events - ApplyChange(new SubsidiaryCreatedEvent(id, subsidiaryID, streetAddress, city, postalCode));
+        }
+    }
+}
