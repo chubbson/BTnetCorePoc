fd023d905706584fffe4f986c12988e9a9d9f292
chubbson <chubbson@gmail.com>
2016-12-22 23:55:38 +0100

    Create Subsidiary Command Added

---
+++ NcCqrsPoc/src/NcCqrsPoc.Domain/Commands/CreateSubsidiaryCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Domain.Commands
+{
+    public class CreateSubsidiaryCommand : BaseCommand
+    {
+        public readonly int SubsidiaryID;
+        public readonly string StreetAddress;
+        public readonly string City;
+        public readonly string PostalCode;
+
+        public CreateSubsidiaryCommand(Guid id, int subsidiaryID, string streetAddress, string city, string postalCode)
+        {
+            Id = id;
+            SubsidiaryID = subsidiaryID;
+            StreetAddress = streetAddress;
+            City = city;
+            PostalCode = postalCode;
+        }
+    }
+}
