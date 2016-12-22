07263bb0f8580f5304de47c4524aa256b43f3d96
chubbson <chubbson@gmail.com>
2016-12-23 00:50:04 +0100

    added AssigEmployeeToSubsidiaryCommand, RemoveEmployeeFromSubsidiaryCommand

---
+++ NcCqrsPoc/src/NcCqrsPoc.Domain/Commands/AssignEmployeeToSubsidiaryCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Domain.Commands
+{
+    public class AssignEmployeeToSubsidiaryCommand : BaseCommand
+    {
+        public readonly int EmployeeID;
+        public readonly int SubsidiaryID;
+
+        public AssignEmployeeToSubsidiaryCommand(Guid id, int subsidiaryID, int employeeID)
+        {
+            Id = id;
+            EmployeeID = employeeID;
+            SubsidiaryID = subsidiaryID;
+        } 
+    }
+}
---
+++ NcCqrsPoc/src/NcCqrsPoc.Domain/Commands/RemoveEmployeeFromSubsidiaryCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Domain.Commands
+{
+    public class RemoveEmployeeFromSubsidiaryCommand : BaseCommand
+    {
+        public readonly int EmployeeID;
+        public readonly int LocationId;
+
+        public RemoveEmployeeFromSubsidiaryCommand(Guid id, int subsidiaryID, int employeeID)
+        {
+            Id = id;
+            EmployeeID = employeeID;
+            SubsidiaryID = subsidiaryID;
+        }
+    }
+}
