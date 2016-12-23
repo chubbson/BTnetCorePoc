68f73a124d893b96ceee8d57519555099334c288
chubbson <chubbson@gmail.com>
2016-12-23 02:55:01 +0100

    Added Employee and Subsidiary Read Model Classes, noteEmployeeRM differs form Employee aggregateroot writeModel

---
+++ NcCqrsPoc/src/NcCqrsPoc.Domain/ReadModel/SubsidiaryRM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Domain.ReadModel
+{
+    public class SubsidiaryRM
+    {
+        public int SubsidiaryID { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public List<int> Employees { get; set; }
+        public Guid AggregateID { get; set; }
+
+        public SubsidiaryRM()
+        {
+            Employees = new List<int>();
+        }
+    }
+}
