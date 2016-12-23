4068a0783d5889d8510e612298b1c0b0c7f54c13
chubbson <chubbson@gmail.com>
2016-12-23 02:38:51 +0100

    Added IBaseRepository interface

---
+++ NcCqrsPoc/src/NcCqrsPoc.Domain/ReadModel/Repos/Interfaces/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Domain.ReadModel.Repos.Interfaces
+{
+    interface IBaseRepository<T>
+    {
+        T GetByID(int id);
+        List<T> GetMultiple(List<int> ids);
+        bool Exist(int id);
+        void Save(T item);
+    }
+}
