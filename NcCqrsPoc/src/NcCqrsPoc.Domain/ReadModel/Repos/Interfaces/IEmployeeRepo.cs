83099847cfcce655ae257447376c9f83eb5c195b
chubbson <chubbson@gmail.com>
2016-12-23 02:55:24 +0100

    Added Employee and Subsidiary Repo classes

---
+++ NcCqrsPoc/src/NcCqrsPoc.Domain/ReadModel/Repos/Interfaces/IEmployeeRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Domain.ReadModel.Repos.Interfaces
+{
+    public interface IEmployeeRepo : IBaseRepo<EmployeeRM>
+    {
+        IEnumerable<EmployeeRM> GetAll();
+    }
+
+
+}
---
+++ NcCqrsPoc/src/NcCqrsPoc.Domain/ReadModel/Repos/Interfaces/ISubsidiaryRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Domain.ReadModel.Repos.Interfaces
+{
+    public interface ISubsidiaryRepo : IBaseRepo<SubsidiaryRM>
+    {
+        IEnumerable<SubsidiaryRM> GetAll();
+        IEnumerable<EmployeeRM> GetEmployees(int subsidiaryID);
+        bool HasEmployee(int locationID, int employeeID);
+    }
+}
