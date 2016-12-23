61f4102511115d0c0d9f1ba2a58beadda828264a
chubbson <chubbson@gmail.com>
2016-12-23 06:46:44 +0100

    [reference lack] Event handler classes added for Employee and Subsidiary, IMapper reference is not added yed. Used for Dependency injection in further commit

---
+++ NcCqrsPoc/src/NcCqrsPoc.Domain/EventHandlers/EmployeeEventHandler.cs
@@ -0,0 +1,43 @@
+﻿using CQRSlite.Events;
+using NcCqrsPoc.Domain.Events.Employees;
+using NcCqrsPoc.Domain.ReadModel;
+using NcCqrsPoc.Domain.ReadModel.Repos.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Domain.EventHandlers
+{
+    public class EmployeeEventHandler : IEventHandler<EmployeeCreatedEvent>
+    {
+        private readonly IMapper _mapper;
+        private readonly IEmployeeRepo _employeeRepo;
+        public EmployeeEventHandler(IMapper mapper, IEmployeeRepo employeeRepo)
+        {
+            _mapper = mapper;
+            _employeeRepo = employeeRepo; 
+        }
+
+        public void Handle(EmployeeCreatedEvent message)
+        {
+            /*
+            var emloyee = new EmployeeRM() { AggregateID = !!!magicstuff!!! ...,
+                                             EmployeeID = message.EmployeeID,
+                                             FirstName = message.FirstName,
+                                             LastName = message.LastName,
+                                             DateOfBirth = message.DateOfBirth,
+                                             JobTitle = message.JobTitle,
+                                             SubsidiaryID = !!!magicstuff!!!... }
+                AggregateId and SubsidiaryId will not be set, should be default Value/null sth like that. 
+                it is not explicity used in employeeRepo save command
+             this magic stuff should be handelt by IMapper.Map method, 
+             IMapper is form AutoMapper
+             Dependency Injection which will setup in part4 
+             does greatly simplifies event hanlder
+             */
+             EmployeeRM employee = _mapper.Map<EmployeeRM>(message);
+            _employeeRepo.Save(employee);
+        }
+    }
+}
---
+++ NcCqrsPoc/src/NcCqrsPoc.Domain/EventHandlers/SubsidiaryEventHandler.cs
@@ -0,0 +1,52 @@
+﻿using CQRSlite.Events;
+using NcCqrsPoc.Domain.Events.Subsidiaries;
+using NcCqrsPoc.Domain.ReadModel;
+using NcCqrsPoc.Domain.ReadModel.Repos.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Domain.EventHandlers
+{
+    public class SubsidiaryEventHandler : IEventHandler<SubsidiaryCreatedEvent>, 
+                                          IEventHandler<EmployeeAssignedToSubsidiaryEvent>, 
+                                          IEventHandler<EmployeeRemovedFromSubsidiaryEvent>
+    {
+        private readonly IMapper _mapper;
+        private readonly ISubsidiaryRepo _subsidiaryRepo;
+        private readonly IEmployeeRepo _employeeRepo;
+        public SubsidiaryEventHandler(IMapper mapper, ISubsidiaryRepo subsidiaryRepo, IEmployeeRepo employeeRepo)
+        {
+            _mapper = mapper;
+            _subsidiaryRepo = subsidiaryRepo;
+            _employeeRepo = employeeRepo;
+        }
+
+        public void Handle(SubsidiaryCreatedEvent message)
+        {
+            // Create a new SubsidiaryDTO object form SubsidiaryCreatedEvent
+            SubsidiaryRM subsidiary = _mapper.Map<SubsidiaryRM>(message);
+            _subsidiaryRepo.Save(subsidiary);
+        }
+
+        public void Handle(EmployeeAssignedToSubsidiaryEvent message)
+        {
+            var subsidiary = _subsidiaryRepo.GetByID(message.NewSubsidiaryID);
+            subsidiary.Employees.Add(message.EmployeeID);
+            _subsidiaryRepo.Save(subsidiary);
+
+            //Find the employee which was assigned to this subsidiary
+            var employee = _employeeRepo.GetByID(message.EmployeeID);
+            employee.SubsidiaryID = message.NewSubsidiaryID;
+            _employeeRepo.Save(employee);
+        }
+
+        public void Handle(EmployeeRemovedFromSubsidiaryEvent message)
+        {
+            var subsidiary = _subsidiaryRepo.GetByID(message.OldSubsidiaryID);
+            subsidiary.Employees.Remove(message.EmployeeId);
+            _subsidiaryRepo.Save(subsidiary);
+        }
+    }
+}
