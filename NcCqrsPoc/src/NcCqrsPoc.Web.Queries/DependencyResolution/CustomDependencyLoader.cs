e6ea6a2b118f4a2acef5f1539b137495eb72d1e2
chubbson <chubbson@gmail.com>
2016-12-28 21:53:40 +0100

    added CustomDependencyLoaderClass with static Load method for DI Repos and Redis Multiplexer

---
+++ NcCqrsPoc/src/NcCqrsPoc.Web.Queries/DependencyResolution/CustomDependencyLoader.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NcCqrsPoc.Domain.ReadModel.Repos;
+using NcCqrsPoc.Domain.ReadModel.Repos.Interfaces;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Web.Queries.DependencyResolution
+{
+    public interface ICustomDependencyLoader { }
+
+    public class CustomDependencyLoader : ICustomDependencyLoader
+    {
+        private CustomDependencyLoader() { }
+
+        public static void LoadCustomDependency(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("localhost"));
+            serviceCollection.AddSingleton<IEmployeeRepo, EmployeeRepo>();
+            serviceCollection.AddSingleton<ISubsidiaryRepo, SubsidiaryRepo>();
+        }
+    }
+}
