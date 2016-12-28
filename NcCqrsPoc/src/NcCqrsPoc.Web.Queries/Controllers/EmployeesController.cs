4ab19d18373a9ab6c2850c70c40416283e6e8c35
chubbson <chubbson@gmail.com>
2016-12-28 21:57:59 +0100

    Added Employees and Subsidiaries Controllers for Query/Readside

---
+++ NcCqrsPoc/src/NcCqrsPoc.Web.Queries/Controllers/EmployeesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NcCqrsPoc.Domain.ReadModel.Repos.Interfaces;
+using NcCqrsPoc.Domain.ReadModel;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NcCqrsPoc.Web.Queries.Controllers
+{
+    // ASP.NET Core - Real-World ASP.NET Core MVC Filtes, lookup reference for async await integration an stuff like that
+    // https://msdn.microsoft.com/en-us/magazine/mt767699.aspx
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeeRepo _employeeRepo;
+
+        public EmployeesController(IEmployeeRepo employeeRepo)
+        {
+            _employeeRepo = employeeRepo;
+        }
+
+        // GET: api/employees/3
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            EmployeeRM employee;
+            try
+            {
+                employee = _employeeRepo.GetByID(id);
+            }
+            catch (ArgumentNullException)
+            {
+                employee = null;
+            }
+
+            //It is possible for GetByID() to return null.
+            //If it does, we return HTTP 400 Bad Request
+            if (employee == null)
+            {
+                return NotFound("No Employee with ID " + id.ToString() + " was found."); //404, -> badrequest = 400 as alternativ
+            }
+
+            // Otherwise, we return the employee
+            return Ok(employee);
+        }
+
+        // GET: api/employees
+        [HttpGet] // Route("all");
+        public IEnumerable<EmployeeRM> Get()
+        {
+            var employees = _employeeRepo.GetAll();
+            return employees;
+        }
+
+        // GET: api/employees/all
+        [HttpGet("all")] // Route("all");
+        public IActionResult GetAll()
+        {
+            var employees = _employeeRepo.GetAll();
+            return Ok(employees);
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NcCqrsPoc/src/NcCqrsPoc.Web.Queries/Controllers/SubsidiariesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NcCqrsPoc.Domain.ReadModel.Repos.Interfaces;
+using NcCqrsPoc.Domain.ReadModel;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NcCqrsPoc.Web.Queries.Controllers
+{
+    [Route("api/[controller]")]
+    public class SubsidiariesController : Controller
+    {
+        private readonly ISubsidiaryRepo _subsidiaryRepo;
+
+        public SubsidiariesController(ISubsidiaryRepo subsidiaryRepo)
+        {
+            _subsidiaryRepo = subsidiaryRepo;
+        }
+
+        // GET: api/subsidiaries/3
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            SubsidiaryRM subsidiary;
+            try
+            {
+                subsidiary = _subsidiaryRepo.GetByID(id);
+            }
+            catch (ArgumentNullException)
+            {
+                subsidiary = null;
+            }
+
+            //It is possible for GetByID() to return null.
+            //If it does, we return HTTP 400 Bad Request
+            if (subsidiary == null)
+            {
+                return NotFound("No subsidiary with ID " + id.ToString() + " was found."); //404, -> badrequest = 400 as alternativ
+            }
+
+            // Otherwise, we return the employee
+            return Ok(subsidiary);
+        }
+
+        // GET: api/subsidiaries
+        [HttpGet] // Route("all");
+        public IEnumerable<SubsidiaryRM> Get()
+        {
+            var subsidiaries = _subsidiaryRepo.GetAll();
+            return subsidiaries;
+        }
+
+        // GET: api/subsidiaries/all
+        [HttpGet("all")] 
+        public IActionResult GetAll()
+        {
+            var employees = _subsidiaryRepo.GetAll();
+            return Ok(employees);
+        }
+
+        // GET: api/subsidiaries/5/employees
+        [HttpGet("{id}/employees")]
+        public IActionResult GetEmployees(int id)
+        {
+            var employees = _subsidiaryRepo.GetEmployees(id);
+            return Ok(employees);
+        }
+    }
+}
