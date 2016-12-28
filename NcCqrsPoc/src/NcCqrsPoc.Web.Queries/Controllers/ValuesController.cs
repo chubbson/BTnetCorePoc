bb3e93f1216763b597c513998fa50687355bec1f
chubbson <chubbson@gmail.com>
2016-12-28 21:57:03 +0100

    Updated Value controller for testing repositories via DI

--- NcCqrsPoc/src/NcCqrsPoc.Web.Queries/Controllers/ValuesController.cs
+++ NcCqrsPoc/src/NcCqrsPoc.Web.Queries/Controllers/ValuesController.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NcCqrsPoc.Domain.ReadModel.Repos.Interfaces;
 
 namespace NcCqrsPoc.Web.Queries.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        
+        private readonly IEmployeeRepo _employeeRepo;
+        public ValuesController(IEmployeeRepo employeeRepo)
+        {
+            _employeeRepo = employeeRepo; 
+        }
+
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<NcCqrsPoc.Domain.ReadModel.EmployeeRM> Get()
         {
-            return new string[] { "value1", "value2" };
+            var allEmployees = _employeeRepo.GetAll();
+            return allEmployees;
+            //return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
