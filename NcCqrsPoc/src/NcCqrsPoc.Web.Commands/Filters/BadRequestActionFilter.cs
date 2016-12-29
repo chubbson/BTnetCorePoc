fa118a04e0f6cda53be0e5e1b2c9cf90f692e026
chubbson <chubbson@gmail.com>
2016-12-30 00:28:56 +0100

    Adding ActionFilterClasses

---
+++ NcCqrsPoc/src/NcCqrsPoc.Web.Commands/Filters/BadRequestActionFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace NcCqrsPoc.Web.Commands.Filters
+{
+    public class BadRequestActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext actionContext)
+        {
+            if (!actionContext.ModelState.IsValid)
+            {
+                actionContext.Result = new BadRequestObjectResult(new ValidationErrorWrapper(actionContext.ModelState));
+                //actionContext.Result = new BadRequestObjectResult(actionContext.ModelState); // ToDo, probalby could remove validationErrorWrapper class. 
+            }
+            //base.OnActionExecuting(actionContext); //ToDo, prbbly remove this base call.
+        }
+    }
+}
---
+++ NcCqrsPoc/src/NcCqrsPoc.Web.Commands/Filters/ValidationErrorWrapper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace NcCqrsPoc.Web.Commands.Filters
+{
+    //Stolen from http://sergeyakopov.com/restful-validation-with-asp-net-web-api-and-fluentvalidation/
+    public class ValidationErrorWrapper
+    {
+        private const string ErrorMessage = "The request is invalid.";
+        private const string MissingPropertyError = "Undefined error.";
+
+        public string Message { get; private set; }
+        public IDictionary<string, IEnumerable<string>> Errors { get; private set; }
+
+        public ValidationErrorWrapper(ModelStateDictionary modelState)
+        {
+            Message = ErrorMessage;
+            SerializeModelState(modelState);
+        }
+
+        public ValidationErrorWrapper(string message, ModelStateDictionary modelState)
+        {
+            Message = message;
+            SerializeModelState(modelState);
+        }
+
+        public void SerializeModelState(ModelStateDictionary modelState)
+        {
+            Errors = new Dictionary<string, IEnumerable<string>>();
+            foreach (var keyModelStatePair in modelState)
+            {
+                var key = keyModelStatePair.Key;
+                var errors = keyModelStatePair.Value.Errors;
+                if (errors != null && errors.Count > 0)
+                {
+                    IEnumerable<string> errorMessages = errors.Select(
+                        error => string.IsNullOrEmpty(error.ErrorMessage)
+                            ? MissingPropertyError
+                            : error.ErrorMessage).ToArray();
+
+                    Errors.Add(key, errorMessages);
+                }
+            }
+        }
+
+    }
+}
