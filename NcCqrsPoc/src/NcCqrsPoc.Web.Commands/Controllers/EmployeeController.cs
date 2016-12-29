40ba3bd105d716bfe24dddde08cee6928bac1f6b
chubbson <chubbson@gmail.com>
2016-12-30 00:30:08 +0100

    Adding Employee and Subsidiary Controller classes

---
+++ NcCqrsPoc/src/NcCqrsPoc.Web.Commands/Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CQRSlite.Commands;
+using AutoMapper;
+using NcCqrsPoc.Web.Commands.Requests.Employees;
+using NcCqrsPoc.Domain.Commands;
+using NcCqrsPoc.Domain.ReadModel.Repos.Interfaces;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NcCqrsPoc.Web.Commands.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        private IMapper _mapper;
+        private ICommandSender _commandSender;
+        private ISubsidiaryRepo _subsidiaryRepo;
+
+        public EmployeeController(ICommandSender commandSender, IMapper mapper, ISubsidiaryRepo subsidiaryRepo)
+        {
+            _commandSender = commandSender;
+            _mapper = mapper;
+            _subsidiaryRepo = subsidiaryRepo;
+        }
+
+        // GET: api/employee/create
+        [HttpPost("create")]
+        //[NcCqrsPoc.Web.Commands.Filters.BadRequestActionFilter]
+        public IActionResult Create(CreateEmployeeRequest request)
+        {
+            var command = _mapper.Map<CreateEmployeeCommand>(request);
+            _commandSender.Send(command);
+
+            var subsidiaryAggregateID = _subsidiaryRepo.GetByID(request.SubsidiaryID).AggregateID;
+
+            var assignCommand = new AssignEmployeeToSubsidiaryCommand(subsidiaryAggregateID, request.SubsidiaryID, request.EmployeeID);
+            _commandSender.Send(assignCommand);
+            return Ok();
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NcCqrsPoc/src/NcCqrsPoc.Web.Commands/Controllers/SubsidiaryController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using CQRSlite.Commands;
+using NcCqrsPoc.Domain.ReadModel.Repos.Interfaces;
+using NcCqrsPoc.Web.Commands.Requests.Subsidiaries;
+using NcCqrsPoc.Domain.Commands;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NcCqrsPoc.Web.Commands.Controllers
+{
+    [Route("api/[controller]")]
+    public class SubsidiaryController : Controller
+    {
+        private IMapper _mapper;
+        private ICommandSender _commandSender;
+        private ISubsidiaryRepo _subsidiaryRepo;
+        private IEmployeeRepo _employeeRepo;
+
+        public SubsidiaryController(ICommandSender commandSender, IMapper mapper, ISubsidiaryRepo subsidiary, IEmployeeRepo employee)
+        {
+            _commandSender = commandSender;
+            _mapper = mapper;
+            _subsidiaryRepo = subsidiary;
+            _employeeRepo = employee;
+        }
+
+        // GET: api/subsidiary/create
+        [HttpPost("create")]
+        //[NcCqrsPoc.Web.Commands.Filters.BadRequestActionFilter]
+        public IActionResult Create(CreateSubsidiaryRequest request)
+        {
+            var command = _mapper.Map<CreateSubsidiaryCommand>(request);
+            _commandSender.Send(command);
+            return Ok();
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
