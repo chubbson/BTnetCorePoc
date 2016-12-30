9c68a06f815ff4993b738df8d3c2d35442db58c6
chubbson <chubbson@gmail.com>
2016-12-30 01:17:16 +0100

    Added AssignEmployeeToSubsidiaryRequest with FluentValidation validator classes

---
+++ NcCqrsPoc/src/NcCqrsPoc.Web.Commands/Requests/Subsidiaries/AssignEmployeeToSubsidiaryRequest.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using NcCqrsPoc.Domain.ReadModel.Repos.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Web.Commands.Requests.Subsidiaries
+{
+    public class AssignEmployeeToSubsidiaryRequest
+    {
+        public int SubsidiaryID { get; set; }
+        public int EmployeeID { get; set; }
+    }
+
+    public class AssignEmployeeToSubsidiaryRequestValidator : AbstractValidator<AssignEmployeeToSubsidiaryRequest>
+    {
+        public AssignEmployeeToSubsidiaryRequestValidator(IEmployeeRepo employeeRepo, ISubsidiaryRepo subsidiaryRepo)
+        {
+            RuleFor(x => x.SubsidiaryID).Must(x => subsidiaryRepo.Exists(x)).WithMessage("No Subsidiary with this ID exists.");
+            RuleFor(x => x.EmployeeID).Must(x => employeeRepo.Exists(x)).WithMessage("No Employee with this ID exists.");
+            RuleFor(x => new { x.SubsidiaryID, x.EmployeeID }).Must(x => !subsidiaryRepo.HasEmployee(x.SubsidiaryID, x.EmployeeID));
+        }
+    }
+}
