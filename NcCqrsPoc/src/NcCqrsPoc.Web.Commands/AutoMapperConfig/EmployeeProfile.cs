33fa1d9e3804de013665b678d84d842a08ccb1e1
chubbson <chubbson@gmail.com>
2017-01-04 14:56:19 +0100

    Added AutoMapper Profile classes

---
+++ NcCqrsPoc/src/NcCqrsPoc.Web.Commands/AutoMapperConfig/EmployeeProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using NcCqrsPoc.Domain.Commands;
+using NcCqrsPoc.Domain.Events.Employees;
+using NcCqrsPoc.Domain.ReadModel;
+using NcCqrsPoc.Web.Commands.Requests.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Web.Commands.AutoMapperConfig
+{
+    public class EmployeeProfile : Profile
+    {
+        public EmployeeProfile()
+        {
+            CreateMap<CreateEmployeeRequest, CreateEmployeeCommand>()
+                .ConstructUsing(x => new CreateEmployeeCommand(Guid.NewGuid(), x.EmployeeID, x.FirstName, x.LastName, x.DateOfBirth, x.JobTitle));
+
+            CreateMap<EmployeeCreatedEvent, EmployeeRM>()
+                .ForMember(dest => dest.AggregateID, opt => opt.MapFrom(src => src.Id));
+        }
+    }
+}
---
+++ NcCqrsPoc/src/NcCqrsPoc.Web.Commands/AutoMapperConfig/SubsidiaryProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using NcCqrsPoc.Domain.Commands;
+using NcCqrsPoc.Domain.Events.Subsidiaries;
+using NcCqrsPoc.Domain.ReadModel;
+using NcCqrsPoc.Web.Commands.Requests.Subsidiaries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Web.Commands.AutoMapperConfig
+{
+    public class SubsidiaryProfile : Profile
+    {
+        public SubsidiaryProfile()
+        {
+            CreateMap<CreateSubsidiaryRequest, CreateSubsidiaryCommand>()
+                .ConstructUsing(x => new CreateSubsidiaryCommand(Guid.NewGuid(), x.SubsidiaryID, x.StreetAddress, x.City, x.PostalCode));
+
+            CreateMap<SubsidiaryCreatedEvent, SubsidiaryRM>()
+                .ForMember(dest => dest.AggregateID, opt => opt.MapFrom(src => src.Id));
+        }
+    }
+}
