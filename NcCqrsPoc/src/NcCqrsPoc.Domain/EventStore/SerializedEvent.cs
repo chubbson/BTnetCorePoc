1d3b50b909f694628da21f15cb3dc7d91a7ef546
chubbson <chubbson@gmail.com>
2017-01-04 14:38:20 +0100

    Added not used Serialized Event Class to Domain Project

---
+++ NcCqrsPoc/src/NcCqrsPoc.Domain/EventStore/SerializedEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Domain.EventStore
+{
+    // ToDo, remove class. Not used actually
+    /*
+    public class SerializedEvent
+    {
+        public Guid EventId { get; set; }
+        public Guid AggregateId { get; set; }
+        public string Type { get; set; }
+        public string Event { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int Version { get; set; }
+    }
+    */
+}
