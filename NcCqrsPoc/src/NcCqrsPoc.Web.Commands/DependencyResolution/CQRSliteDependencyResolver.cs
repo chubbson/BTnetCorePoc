49bf2fb3df685dddc3e13e322b9ac8da519b4e59
chubbson <chubbson@gmail.com>
2017-01-04 14:55:41 +0100

    Added, CQRSliteDependencyResolver class, used for Bus Regristrar functionality

---
+++ NcCqrsPoc/src/NcCqrsPoc.Web.Commands/DependencyResolution/CQRSliteDependencyResolver.cs
@@ -0,0 +1,45 @@
+﻿using CQRSlite.Config;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NcCqrsPoc.Web.Commands.DependencyResolution
+{
+    public class CQRSliteDependencyResolver : IServiceLocator
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public CQRSliteDependencyResolver(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public T GetService<T>()
+        {
+            return (T)GetService(typeof(T));
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == null)
+            {
+                return null;
+            }
+            try
+            {
+                return _serviceProvider.GetService(serviceType);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _serviceProvider.GetServices(serviceType);
+        }
+    }
+}
